7cba908da679f0158b4f9b874d8a9fff5a4090c2
sbb3391 <sbb33391@gmail.com>
2021-05-29 13:20:01 -0400

    update goodDog button is working

--- src/index.js
+++ src/index.js
@@ -42,6 +42,7 @@ function showDogInfoEventHandler(event) {
   }
 
   function showDog(json) {
+    debugger;
     const dogInfo = document.querySelector("div#dog-info");
 
     const img = document.createElement("img");
@@ -50,11 +51,41 @@ function showDogInfoEventHandler(event) {
 
     img.setAttribute("src", json.image);
     h2.innerText = json.name;
+    button.dataset.id = json.id
     json.isGoodDog ? button.innerText = "Good Dog!" : button.innerText = "Bad Dog!" 
 
     dogInfo.appendChild(img)
     dogInfo.appendChild(h2);
     dogInfo.appendChild(button);
+
+    button.addEventListener("click", goodDogButtonHandler)
+
+    function goodDogButtonHandler(e) {
+      goodDogValue = ""
+      if (e.target.innerText === "Good Dog!") {
+        e.target.innerText = "Bad Dog!"
+        goodDogValue = false
+      }
+      else {
+        e.target.innerText = "Good Dog!"
+        goodDogValue = true
+      }
+
+      toggleGoodDog(e.target.dataset.id, goodDogValue)
+    }
   }
 }
 
+function toggleGoodDog(id, goodDogValue) {
+  const options = {
+    method: "PATCH",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({
+      isGoodDog: goodDogValue
+    })
+  }
+  return fetch(DOG_BASE_URL + `/${id}`)
+  .then(resp => resp.json())
+}
